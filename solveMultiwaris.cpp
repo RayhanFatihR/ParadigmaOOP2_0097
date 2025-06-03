bd21463565b9ffbde3f7b73032d75dff212930e4
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-03 15:17:41 +0700

    Tahap 2

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -20,4 +20,14 @@ class pekerja : public orang {
         {
             cout << "pekerja dibuat\n" << endl;
         }
-}
+};
+
+class pelajar : public orang {
+    public:
+
+        pelajar(int pUmur) :
+            orang(pUmur)
+        {
+            cout << "pelajar dibuat\n" << endl;
+        }
+};
